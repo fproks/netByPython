7edd957bee8576f078172ace356a506b3cafc605
fproks <fproks@126.com>
2017-03-30 08:42:56 +0800

    添加解释文档

--- test/test1/tcpdump.py
+++ test/test1/tcpdump.py
@@ -8,6 +8,9 @@
 '''
 '''
 python执行shell语句，将结果通过管道进行输出
+暂时无法通过java直接调用shell命令，因为shell命令需要root权限
+该方法仅仅是得到了print数据，还需要对数据进行切割，分解，存储
+不同的数据，拥有不相同的数据格式，因此还需要对命令进行分解
 '''
 import sys
 import subprocess
@@ -15,6 +18,7 @@ sub=subprocess.Popen('tcpdump -n -nn -vv -XX -e -A',shell=True,stdout=subprocess
 while sub.poll()==None:
     sys.stdout.flush()
     print sub.stdout.readline(),
+
 print sub.returncode
 
 
